c72c131f25e09077028c580ff0c635b93cb3e54a
Samuel Vojtáš <samuel.vojtas@gmail.com>
2024-11-07 21:02:34 +0100

    add helpers script with helper functions and classes

---
+++ helpers.py
@@ -0,0 +1,62 @@
+from colorama import Fore, Style
+import argparse
+import sys
+import yaml
+
+from dataclasses import dataclass
+
+EXIT_FAILURE = 1
+
+
+def inform(msg):
+    """
+    Print inform message to the STDOUT
+    """
+    print(Fore.BLUE + "  [*] " + Style.RESET_ALL + msg)
+
+def success(msg):
+    """
+    Print success message to the STDOUT
+    """
+    print(Fore.GREEN + "  [*] " + Style.RESET_ALL + msg)
+
+def error(msg):
+    """
+    Print error message to the STDERR
+    """
+    print(Fore.RED + "  [*] " + Style.RESET_ALL + msg, file=sys.stderr)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Backdoor for face-recognition algorithm")
+
+    parser.add_argument("-l", "--load", help="load the old model", action="store_true")
+
+    parser.add_argument("-i", "--impostor", help="name of the impostor", type=str)
+
+    parser.add_argument("-v", "--victim", help="name of the victim", type=str)
+
+    args, unknown_args = parser.parse_known_args()
+
+    if unknown_args:
+        error("Unknown args given")
+
+    return args
+
+@dataclass
+class Config:
+    """
+    Class or storing the training configuration
+    """
+    batch_size: int
+    learning_rate: float
+    min_delta: float
+    epochs: int
+
+    def __init__(self, path: str) -> None:
+        with open(path, "r") as fp:
+            config = yaml.safe_load(fp)
+
+        self.batch_size = config["training"]["batch_size"]
+        self.learning_rate = config["training"]["learning_rate"]
+        self.min_delta = config["training"]["min_delta"]
+        self.epochs = config["training"]["epochs"]
